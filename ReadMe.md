f1a7b59ce6a91281a5e8953b21015b8b154a5845
lincheng <lincheng930901@gmail.com>
2021-11-18 09:42:20 +0900

    commit

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+Pepeswap frontend
+
+Pluto-protocol forked from Uniswap protocol intended to be a cross chain DEX and AmM.
---
+++ src/data/BaseTokenList.ts
@@ -0,0 +1,91 @@
+export const baseTokenList = {
+  name: 'BEP20 Token List',
+  timestamp: '2020-11-08T09:11:15.278Z',
+  version: {
+    major: 1,
+    minor: 9,
+    patch: 29
+  },
+  tags: {},
+  logoURI:
+    'https://francis-song.github.io/test-default-token-list/images/0x0c3ea5aac39101e5b3989b3f19181d8591b734d0.png',
+  keywords: ['pepeswap', 'default'],
+  tokens: [
+    {
+      name: 'BUSD Token',
+      address: '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56',
+      symbol: 'BUSD',
+      decimals: 18,
+      chainId: 56,
+      logoURI: 'https://tokens.bscswap.com/images/0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56.png'
+    },
+    {
+      name: 'Wrapped BNB',
+      address: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+      symbol: 'WBNB',
+      decimals: 18,
+      chainId: 56,
+      logoURI: 'https://tokens.bscswap.com/images/0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c.png'
+    },
+    {
+      name: 'Pluto Token',
+      address: '0x0c3ea5aac39101e5b3989b3f19181d8591b734d0',
+      symbol: 'PLUTO',
+      decimals: 9,
+      chainId: 56,
+      logoURI:
+        'https://francis-song.github.io/test-default-token-list/images/0x0c3ea5aac39101e5b3989b3f19181d8591b734d0.png'
+    },
+    {
+      name: 'Pepe Token',
+      address: '0x817cc27fae81dc224c9d23f8339b5fb6a0badeb3',
+      symbol: 'PEPE',
+      decimals: 18,
+      chainId: 56,
+      logoURI:
+        'https://francis-song.github.io/test-default-token-list/images/0x817cc27fae81dc224c9d23f8339b5fb6a0badeb3.png'
+    },
+    {
+      name: 'Wrapped BNB',
+      address: '0x094616f0bdfb0b526bd735bf66eca0ad254ca81f',
+      symbol: 'WBNB',
+      decimals: 18,
+      chainId: 97,
+      logoURI: 'https://tokens.bscswap.com/images/0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd.png'
+    },
+    {
+      name: 'Pluto Token',
+      address: '0x569f72E810452Bf676bB4C67aa2C20CE672DFbD9',
+      symbol: 'PLUTO',
+      decimals: 18,
+      chainId: 97,
+      logoURI:
+        'https://francis-song.github.io/test-default-token-list/images/0x0c3ea5aac39101e5b3989b3f19181d8591b734d0.png'
+    },
+    {
+      name: 'Pepe Token',
+      address: '0x699Ee768A8A2850eFdA67f5e89b12a7e20Fd631A',
+      symbol: 'PEPE',
+      decimals: 18,
+      chainId: 97,
+      logoURI:
+        'https://francis-song.github.io/test-default-token-list/images/0x817cc27fae81dc224c9d23f8339b5fb6a0badeb3.png'
+    },
+    {
+      name: 'BUSD Token',
+      address: '0xF16f1C5A8f53d390442B3718bc3697AE56971792',
+      symbol: 'BUSD',
+      decimals: 18,
+      chainId: 97,
+      logoURI: 'https://tokens.bscswap.com/images/0xeD24FC36d5Ee211Ea25A80239Fb8C4Cfd80f12Ee.png'
+    },
+    {
+      name: 'WETH Token',
+      address: '0xF282BA187F0fF04962145FB504ba74E6F1caCC15',
+      symbol: 'WETH',
+      decimals: 18,
+      chainId: 97,
+      logoURI: 'https://tokens.bscswap.com/images/0xeD24FC36d5Ee211Ea25A80239Fb8C4Cfd80f12Ee.png'
+    }
+  ]
+}
